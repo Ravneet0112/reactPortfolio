4093304580ce5316276f449e074c55710bbc963f
Ravneet Kaur Sammewali <ravneet.sammewali01@gmail.com>
2023-03-24 21:51:17 -0400

    Modified project navbar

--- react-portfolio/src/components/pages/Blog.js
+++ react-portfolio/src/components/pages/Blog.js
@@ -3,11 +3,12 @@ import Budget from '../../Assets/Budget.png';
 import Password from '../../Assets/Password.png';
 import Stock from '../../Assets/Stock.png';
 import Weather from '../../Assets/Weather.png';
+import ecommerce from '../../Assets/ecommerce.png';
 
 const styles={
   divStyle: {
     padding: '60px',
-    width: '80%',
+    width: '70%',
     alignitems: 'center',
     margin: 'auto',
     background: 'lightgrey',
@@ -17,7 +18,6 @@ const styles={
     display: 'grid',
     gridTemplateColumns: 'repeat(2, 1fr)',
     gridGap: '20px',
-    gridTemplateRows: 'Auto',
     width: '100%',
     innerHeight: '100%',
   },
@@ -29,23 +29,32 @@ export default function Portfolio() {
       <h2>My work</h2>
       <hr></hr>
 
-      <div className='wrapper'>
+      <div className='wrapper' style={styles.divwrapper}>
         <div >
-          <img src={Budget} alt='Budget Tracker' />
-        <a href="https://github.com/cdennis27/budgettrackergroup2">Budget Tracker</a>
+          
+        <a href="https://github.com/cdennis27/budgettrackergroup2"><img src={Budget} alt='Budget Tracker' /></a>
+        <div className="bottom-center">Budget Tracker</div>
 
         </div>
         <div>
-          <img src={Password} alt='Password Generator' />
-          <a href="https://github.com/Ravneet0112/ravneet-password-generator">Password Generator</a>
+          <a href="https://github.com/Ravneet0112/ravneet-password-generator" >
+          <img src={Password} alt='Password Generator'/> </a>      
+          <div className="bottom-center">Password Generator</div>
           </div>
         <div>
-          <img src={Stock} alt='Stock Search' />
-          <a href="https://github.com/areyno20/stock-app"> Stock Search</a>
+          
+          <a href="https://github.com/areyno20/stock-app"><img src={Stock} alt='Stock Search' /></a>
+          <div className="bottom-center">Stock Search</div>
           </div>
         <div>
-          <img src={Weather} alt='Weather Dashboard' />
-          <a href="https://github.com/Ravneet0112/ravneet-weather-app">Weather App</a>
+          
+          <a href="https://github.com/Ravneet0112/ravneet-weather-app"><img src={Weather} alt='Weather Dashboard' /></a>
+          <div className="bottom-center">Weather App</div>
+        </div>
+
+        <div>
+          <a href="https://github.com/Ravneet0112/e-commerce"><img src={ecommerce} alt='E-Commerce' /></a>
+          <div className="bottom-center">E-Commerce</div>
         </div>
         
         
